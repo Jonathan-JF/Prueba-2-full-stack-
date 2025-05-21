22ede5cb8a38723af1f2532c2fb8f691b475c7d2
Admin <jonathan30012@hotmail.com>
2025-05-20 23:19:01 -0400

    Se agrega el proyecto

---
+++ src/main/java/com/ethicaia/ethicaia/EthicaiaApplication.java
@@ -0,0 +1,13 @@
+package com.ethicaia.ethicaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthicaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthicaiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethicaia/ethicaia/EthicaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethicaia.ethicaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthicaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
